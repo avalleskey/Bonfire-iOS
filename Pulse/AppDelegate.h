4994845173193e8192d260458e48ce96ebf39c7f
Austin Valleskey <austin@hallway.app>
2018-09-16 14:53:57 -0500

    Initial Commit

---
+++ Pulse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pulse
+//
+//  Created by Austin Valleskey on 9/16/18.
+//  Copyright © 2018 Austin Valleskey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

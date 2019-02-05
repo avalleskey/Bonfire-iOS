3008725a28c78293281eb115986d9508c11b4551
Austin Valleskey <austin.valleskey@gmail.com>
2019-02-05 12:01:33 -0600

    Get up to date pt 3

---
+++ Pulse/Controllers/Settings/Legal/Open Source Libraries/OpenSourceLibrariesTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  OpenSourceLibrariesTableViewController.h
+//  Pulse
+//
+//  Created by Austin Valleskey on 12/29/18.
+//  Copyright © 2018 Austin Valleskey. All rights reserved.
+//
+
+#import "SmartListTableViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OpenSourceLibrariesTableViewController : SmartListTableViewController <SmartListDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END

c570d14f3682bbb6010bccc150221de29ef6cb93
James Dale <git@jamesdale.com.au>
2020-06-20 05:22:25 +1000

    New Project

---
+++ Bonfire/Bonfire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bonfire
+//
+//  Created by James Dale on 19/6/20.
+//  Copyright © 2020 James Dale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

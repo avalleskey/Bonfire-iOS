21281ef2fe9d1d1b49a11585224759f939064afa
Austin Valleskey <austin.valleskey@gmail.com>
2020-07-29 00:04:55 -0400

    Add  haptics flag to BFBouncyButton class

--- Bonfire/Bonfire/Views/Button/BFBouncyButton.swift
+++ Bonfire/Bonfire/Views/Button/BFBouncyButton.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class BFBouncyButton: UIButton {
 
+    var haptics: Bool = true
+    
     override init(frame: CGRect) {
         super.init(frame: .zero)
 
@@ -26,8 +28,10 @@ class BFBouncyButton: UIButton {
     }
 
     @objc private func animateDown(sender: UIButton) {
-        let generator = UISelectionFeedbackGenerator()
-        generator.selectionChanged()
+        if haptics {
+            let generator = UISelectionFeedbackGenerator()
+            generator.selectionChanged()
+        }
 
         animate(sender, transform: CGAffineTransform.identity.scaledBy(x: 0.9, y: 0.9))
     }

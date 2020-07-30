9205df58e7c8a224c8d587f2604f8d92b0033985
James Dale <git@jamesdale.com.au>
2020-07-31 09:41:15 +1000

    Circular Button

--- Bonfire/Bonfire/Views/Button/BFCircularButton.swift
+++ Bonfire/Bonfire/Views/Button/BFCircularButton.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2020 Ingenious. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class BFCircularButton: UIButton {
+    private func updateRadius() {
+        layer.cornerRadius = frame.width / 2
+        layer.masksToBounds = true
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        updateRadius()
+    }
+
+    override func updateConstraints() {
+        super.updateConstraints()
+        translatesAutoresizingMaskIntoConstraints = false
+        widthAnchor.constraint(equalTo: heightAnchor).isActive = true
+    }
+}

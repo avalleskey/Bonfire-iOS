99d4bdcd4702b0b561e443101f85e7693ebfac3c
James Dale <git@jamesdale.com.au>
2020-07-31 09:40:08 +1000

    Camp Updates

--- Bonfire/Bonfire/View Controllers/Camp/CampViewController.swift
+++ Bonfire/Bonfire/View Controllers/Camp/CampViewController.swift
@@ -6,4 +6,38 @@
 //  Copyright © 2020 Ingenious. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class CampViewController: UIViewController {
+
+    private let feedTableView = BFFeedTableViewController()
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        
+        view.backgroundColor = Constants.Color.brand
+        navigationItem.title = "Camp Test"
+        
+        addChild(feedTableView)
+        view.addSubview(feedTableView.view)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        navigationController?.interactivePopGestureRecognizer?.delegate = self
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
+
+extension CampViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        true
+    }
+}
+
--- Bonfire/Bonfire/View Controllers/Camp/Members/CampMemberCell.swift
+++ Bonfire/Bonfire/View Controllers/Camp/Members/CampMemberCell.swift
@@ -6,4 +6,58 @@
 //  Copyright © 2020 Ingenious. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class CampMemberCell: UITableViewCell {
+    
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
+    let primaryLabel: UILabel = {
+        let label = UILabel()
+        label.text = "John Smith"
+        label.font = UIFont.systemFont(ofSize: 17, weight: .bold).rounded()
+        label.textColor = Constants.Color.primary
+        return label
+    }()
+    
+    let secondaryLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Joined 2d ago"
+        label.font = UIFont.systemFont(ofSize: 15, weight: .bold).rounded()
+        label.textColor = Constants.Color.secondary
+        return label
+    }()
+    
+    let stackView = UIStackView()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(profileImageView)
+        addSubview(stackView)
+        
+        stackView.addArrangedSubview(primaryLabel)
+        stackView.addArrangedSubview(secondaryLabel)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func updateConstraints() {
+        super.updateConstraints()
+        
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            profileImageView.leadingAnchor.constraint(equalTo: leadingAnchor,
+                                                      constant: 12),
+            
+            stackView.leadingAnchor.constraint(equalTo: profileImageView.trailingAnchor,
+                                               constant: 12)
+        ])
+    }
+}

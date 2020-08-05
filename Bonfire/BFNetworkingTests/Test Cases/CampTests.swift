ad96570732e142f48e3903ccf0a32e265f5dd782
James Dale <git@jamesdale.com.au>
2020-08-06 05:45:26 +1000

    Improve test coverage

---
+++ Bonfire/BFNetworkingTests/Test Cases/CampTests.swift
@@ -0,0 +1,22 @@
+//
+//  CampTests.swift
+//  BFNetworkingTests
+//
+//  Created by James Dale on 6/8/20.
+//  Copyright © 2020 Ingenious. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+@testable import BFNetworking
+
+final class CampTests: BFNetworkingTestCase {
+    
+    private let client = APIClient()
+
+    func testMyCamps() throws {
+        apiTest(client: client, request: MyCampsRequest())
+    }
+    
+}

a4e0bab24a4bc5679cda938ad1db655656c1fd6a
Daniel Gauthier <daniel.m.gauthier@icloud.com>
2020-09-02 17:18:37 -0400

    Add some simple loading and empty states to feed.

--- Bonfire/Bonfire/View Controllers/Dan's new feed stuff/NewFeedViewController.swift
+++ Bonfire/Bonfire/View Controllers/Dan's new feed stuff/NewFeedViewController.swift
@@ -14,6 +14,8 @@ final class NewFeedViewController: UIViewController {
 
     private let navigationView = NavigationView(color: .systemBackground, leftButtonType: .status(emoji: "🥳"), rightButtonType: .bell, titleImage: .dummyAvatar)
     private let tableView: UITableView = .make(cellReuseIdentifier: FeedCell.reuseIdentifier, cellClass: FeedCell.self, topOffset: NavigationView.coreHeight)
+    private let loadingIndicator = UIActivityIndicatorView(style: .large, isAnimating: true, hidesWhenStopped: true)
+    private let emptyStateMessageView = EmptyStateMessageView(title: "Nothing to show", subtitle: "Start by joining some camps!")
     private var dataSource: UITableViewDiffableDataSource<Int, Post>!
     private var posts: [Post] = []
     private let controller = StreamController()
@@ -34,9 +36,24 @@ final class NewFeedViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
         setUpTableView()
         setUpNavigationView()
+        setUpLoadingIndicator()
+        setUpEmptyStateMessageView()
         refreshData()
+    }
+
+    private func setUpTableView() {
+        view.addSubview(tableView)
+        constrain(tableView) {
+            $0.top == $0.superview!.safeAreaLayoutGuide.top
+            $0.leading == $0.superview!.leading
+            $0.trailing == $0.superview!.trailing
+            $0.bottom == $0.superview!.bottom
+        }
+
+        tableView.alpha = 0
     }
 
     private func setUpNavigationView() {
@@ -50,14 +67,24 @@ final class NewFeedViewController: UIViewController {
         navigationView.managingScrollView = tableView
     }
 
-    private func setUpTableView() {
-        view.addSubview(tableView)
-        constrain(tableView) {
-            $0.top == $0.superview!.safeAreaLayoutGuide.top
-            $0.leading == $0.superview!.leading
-            $0.trailing == $0.superview!.trailing
-            $0.bottom == $0.superview!.bottom
+    private func setUpLoadingIndicator() {
+        view.addSubview(loadingIndicator)
+        constrain(loadingIndicator) {
+            $0.centerX == $0.superview!.centerX
+            $0.centerY == $0.superview!.centerY + (NavigationView.coreHeight / 2)
         }
+    }
+
+    private func setUpEmptyStateMessageView() {
+        view.addSubview(emptyStateMessageView)
+        constrain(emptyStateMessageView) {
+            $0.centerX == $0.superview!.centerX
+            $0.leading >= $0.superview!.leading + 16
+            $0.trailing <= $0.superview!.trailing - 16
+            $0.centerY == $0.superview!.centerY + (NavigationView.coreHeight / 2)
+        }
+
+        emptyStateMessageView.alpha = 0
     }
 
     private func refreshData() {
@@ -65,6 +92,14 @@ final class NewFeedViewController: UIViewController {
             DispatchQueue.main.async {
                 self.posts = posts
                 self.updateDataSource(skipAnimation: true)
+                UIView.animate(withDuration: 0.2, animations: {
+                    if posts.isEmpty {
+                        self.emptyStateMessageView.alpha = 1.0
+                    } else {
+                        self.tableView.alpha = 1.0
+                    }
+                    self.loadingIndicator.alpha = 0.0
+                }, completion: nil)
             }
         }
     }
